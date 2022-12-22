7672a4288ee3d2e93ff500bce23a474fab6e65bd
gholam reza <102972340+gholamreza55@users.noreply.github.com>
2022-12-21 23:52:51 -0800

    switch jadid

--- src/main/java/nouroz/HaftSin.java
+++ src/main/java/nouroz/HaftSin.java
@@ -2,28 +2,39 @@ package nouroz;
 
 public class HaftSin {
     public String ayaJoziazanast(String sib) {
-        String natije = "";
-        switch (sib){
-            case "Sib":
-            case "Sir":
-            case "Samanou":
-            case "Serke":
-            case "Seke":
-            case "Somagh":
-            case "Senjed":
-                natije = "Ejbari";
-                   break;
-            case "mahi":
-            case "sham":
-            case "saat":
-            case "ayne":
-            case "shirini":
-                natije = "Ekhtyari";
-                break;
-            default:
-                natije = "jozi az haft sin nist";
-                break;
-        }
-        return natije;
+//        String natije = "";
+//        switch (sib){
+//            case "Sib":
+//            case "Sir":
+//            case "Samanou":
+//            case "Serke":
+//            case "Seke":
+//            case "Somagh":
+//            case "Senjed":
+//                natije = "Ejbari";
+//                   break;
+//            case "mahi":
+//            case "sham":
+//            case "saat":
+//            case "ayne":
+//            case "shirini":
+//                natije = "Ekhtyari";
+//                break;
+//            default:
+//                natije = "jozi az haft sin nist";
+//                break;
+//        }
+//        return natije;
+//
+        return switch (sib) {
+            case "Sib", "Sir", "Samanou", "Serke", "Seke", "Somagh", "Senjed" -> "Ejbari";
+            case "mahi", "sham", "saat", "ayne", "shirini" ->
+                    {
+                        System.out.println("Momkene bazi ashya faramoosh shode bashe");
+                        yield "Ekhtyari";
+
+                    }
+            default -> "jozi az haft sin nist";
+        };
     }
 }
